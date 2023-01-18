697e094af58c14d0dd3dd8fdc929198daf5d31cc
Daniel Mckenzie <DanielMckenzie@users.noreply.github.com>
2023-01-18 15:51:10 -0700

    Fixed data generation problems

--- source/GenerateShortestPathData.py
+++ source/GenerateShortestPathData.py
@@ -2,9 +2,9 @@ import numpy as np
 import torch
 import networkx as nx
 from torch.utils.data import Dataset, TensorDataset, DataLoader
-from torch.utils.data.dataset import random_split
-from torch_Dijkstra import Dijkstra
-from utils import Node_to_Edge
+from torch.utils.data.dataset import random_split, Subset
+from source.torch_Dijkstra import Dijkstra
+from source.utils import Node_to_Edge
 
 def create_shortest_path_data(m, train_size, test_size, context_size):
     '''
@@ -54,10 +54,14 @@ def create_shortest_path_data(m, train_size, test_size, context_size):
         # place into the Paths_List
         Paths_List_V.append(torch.from_numpy(path_v))
         
-        # Enocde edge description of shortest path as a vector
+        # Encode edge description of shortest path as a vector
         path_vec_e = torch.zeros(len(Edges))
         for i in range(len(path_e)-1):
-            path_vec_e[Edges.index((path_e[i], path_e[i+1]))] = 1
+            try:
+                path_vec_e[Edges.index((path_e[i], path_e[i+1]))] = 1
+            except:
+                path_vec_e[Edges.index((path_e[i+1], path_e[i]))] = 1
+                print('path ' + str(context[0]) + ' involves backtracking')
         Paths_List_E.append(path_vec_e)
         
     ## convert Paths_List to a tensor
@@ -80,7 +84,13 @@ def create_shortest_path_data(m, train_size, test_size, context_size):
     # Construct and return dataset.
     dataset_v = TensorDataset(Context.float(), Paths_V.float())
     dataset_e = TensorDataset(Context.float(), Paths_E.float())
-    train_dataset_v, test_dataset_v = random_split(dataset_v, [train_size, test_size])
-    train_dataset_e, test_dataset_e = random_split(dataset_e, [train_size, test_size])
+    train_dataset_v = Subset(dataset_v, range(train_size))
+    test_dataset_v = Subset(dataset_v, range(train_size, train_size + test_size))
+    train_dataset_e = Subset(dataset_e, range(train_size))
+    test_dataset_e = Subset(dataset_e, range(train_size, train_size + test_size))
+    # avoid random_split as we want the (context, path) pairs to be in the same order
+    # for the vertex (_v) and edge (_e) datasets.
+# train_dataset_v, test_dataset_v = random_split(dataset_v, [train_size, test_size], generator=torch.Generator().manual_seed(42))
+#  train_dataset_e, test_dataset_e = random_split(dataset_e, [train_size, test_size], generator=torch.Generator().manual_seed(42))
     return train_dataset_v, test_dataset_v, train_dataset_e, test_dataset_e, WW, A.float(), b.float(), num_edges, Edges
         
