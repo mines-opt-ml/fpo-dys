2f150d3d00fe4192fb63eb82bd3ce25acc98803b
DanielMckenzie <mckdan002@gmail.com>
2024-01-04 13:05:15 -0700

    fixed shortest path data generation

---
+++ src/shortest_path/generate_shortest_path_data.py
@@ -0,0 +1,88 @@
+from sklearn.model_selection import train_test_split
+from pyepo.model.grb import shortestPathModel
+import pyepo
+import torch
+import dill
+import argparse
+import os
+
+# generate actual data
+def main(args):
+    
+    # unpack args
+    grid_size = args.grid_size
+    grid = (grid_size, grid_size)
+    num_data = args.num_data
+    num_feat = args.num_feat
+
+    print('Generating training data for shortest path with '+str(grid_size)
+          + '-by-' + str(grid_size) + ' grid.')
+    
+    contexts_numpy, costs_numpy = pyepo.data.shortestpath.genData(num_data, num_feat, grid, deg=4, noise_width= 0.5)
+    
+    # split train test data
+    d_train, d_test_val, w_train, w_test_val = train_test_split(contexts_numpy, costs_numpy, test_size=200)
+    d_test, d_val, w_test, w_val = train_test_split(d_test_val, w_test_val, test_size=100)
+    
+    # Define PyEPO model
+    optmodel = shortestPathModel(grid)
+
+    # get optDataset
+    dataset_train = pyepo.data.dataset.optDataset(optmodel, d_train, w_train)
+    dataset_test = pyepo.data.dataset.optDataset(optmodel, d_test, w_test)
+    dataset_val = pyepo.data.dataset.optDataset(optmodel, d_val, w_val)
+
+    # get edges/arcs
+    edges = optmodel.arcs
+
+    # contruct edge-node incidence matrix A and flow vector b
+    A = torch.zeros((grid_size**2, len(edges)))
+    for j,e in enumerate(edges):
+        ind0 = e[0]
+        ind1 = e[1]
+        A[ind0, j] = -1.
+        A[ind1, j] = +1.
+
+    b = torch.zeros(grid_size**2)
+    b[0] = -1.
+    b[-1] = 1.
+
+    print(A)
+    # Remove the gurobi model before saving
+    dataset_train.model = None
+    dataset_test.model = None
+    dataset_val.model = None
+
+    # Package into a dictionary
+    state = {'edges': edges,
+             'A': A,
+             'b': b,
+             'contexts_numpy': contexts_numpy,
+             'costs_numpy'   : costs_numpy,
+             'dataset_train' : dataset_train,
+             'dataset_test'  : dataset_test,
+             'dataset_val'   : dataset_val, 
+            }
+
+    # Save and finish up
+    print('Finished building dataset')
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+    
+    state_path = args.save_dir + 'shortest_path_training_data_' + str(grid_size) + '.p'
+    dill.dump(state, open( state_path, "wb" ) )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='generate shortest path data')
+    parser.add_argument('--num_data', type=int, default=1000)
+    parser.add_argument('--num_feat', type=int, default=5)
+    parser.add_argument('--grid_size', type=int, default=5)
+    parser.add_argument('--save_dir', type=str, default='./src/shortest_path/shortest_path_data/')
+    args = parser.parse_args()
+    main(args)
+
+
+    
+
+    
+    
---
+++ src/shortest_path/utils.py
@@ -0,0 +1,71 @@
+import gurobipy as gp
+from gurobipy import GRB
+from pyepo.model.grb import optGrbModel
+
+class shortestPathModel(optGrbModel):
+    '''
+    Custom optModel class. Code written by Bo Tang as part of PyEPO package.
+    '''
+
+    def __init__(self, grid_size):
+        self.grid_size = grid_size
+        self.grid = (grid_size, grid_size)
+        self.arcs = self._getArcs()
+        super().__init__()
+
+    def _getArcs(self):
+        """
+        A helper method to get list of arcs for grid network
+
+        Returns:
+            list: arcs
+        """
+        arcs = []
+        for i in range(self.grid[0]):
+            # edges on rows
+            for j in range(self.grid[1] - 1):
+                v = i * self.grid[1] + j
+                arcs.append((v, v + 1))
+            # edges in columns
+            if i == self.grid[0] - 1:
+                continue
+            for j in range(self.grid[1]):
+                v = i * self.grid[1] + j
+                arcs.append((v, v + self.grid[1]))
+        return arcs
+
+    def _getModel(self):
+        """
+        A method to build Gurobi model
+
+        Returns:
+            tuple: optimization model and variables
+        """
+        # create a model
+        m = gp.Model("shortest path")
+        # varibles
+        x = m.addVars(self.arcs, name="x")
+        # sense
+        m.modelSense = GRB.MINIMIZE
+        # flow conservation constraints
+        for i in range(self.grid[0]):
+            for j in range(self.grid[1]):
+                v = i * self.grid[1] + j
+                expr = 0
+                for e in self.arcs:
+                    # flow in
+                    if v == e[1]:
+                        expr += x[e]
+                    # flow out
+                    elif v == e[0]:
+                        expr -= x[e]
+                # source
+                if i == 0 and j == 0:
+                    m.addConstr(expr == -1)
+                # sink
+                elif i == self.grid[0] - 1 and j == self.grid[0] - 1:
+                    m.addConstr(expr == 1)
+                # transition
+                else:
+                    m.addConstr(expr == 0)
+        return m, x
