9bb542685b565e1f08bc1f39bd85d19f76ca9150
DanielMckenzie <mckdan002@gmail.com>
2024-01-27 14:27:30 -0700

    Wrote bash file for knapsack experiment

---
+++ knapsack_experiment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define the model types
+model_types="DYS CVX PertOpt BBOpt"
+num_items="20 30 40 50 60"
+reps="1 2 3"
+data_dir="./src/knapsack/knapsack_data"
+weights_dir="./src/knapsack/saved_weights"
+results_dir="./src/knapsack/results"
+
+# Train for each model type and grid size
+for num_item in $num_items
+do
+    for rep in $reps
+    do
+        for model_type in $model_types
+        do
+            rep_data_dir="${data_dir}/$rep"
+            rep_weights_dir="${weights_dir}/$rep"
+            rep_results_dir="${results_dir}/$rep"
+            python -m src.knapsack.train --model_type $model_type --num_item $num_item --num_data 1000 --data_dir $rep_data_dir --results_dir $rep_results_dir --device mps
+            echo "$num_item"
+        done
+    done
+done
